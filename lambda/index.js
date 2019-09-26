fda943dbf699bc90934a1b163e5068f3095c06f5
syntactic-salt <kennethpereira@gmail.com>
2019-09-25 23:50:23 -0400

    set cache-control header

--- lambda/index.js
+++ lambda/index.js
@@ -4,6 +4,8 @@ const url = require('url');
 const server = awsServerlessExpress.createServer((req, res) => {
     const parsedURL = url.parse(req.url);
 
+    res.setHeader('Cache-Control', 'no-cache');
+
     if (/\/[0-9]{4}\/[0-9]{2}\/[0-9]+/.test(parsedURL.pathname)) {
         require('./pages/[year]/[month]/[showID]').render(req, res);
     } else {
